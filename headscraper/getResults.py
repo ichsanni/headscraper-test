619fa59acc5e21a174f694efbd700cfeb43d6f61
ichsanni <48359338+ichsanni@users.noreply.github.com>
2020-06-04 21:29:32 +0700

    hourly update

--- headscraper/getResults.py
+++ headscraper/getResults.py
@@ -38,7 +38,7 @@ class GetResults():
                 existing_words = json.load(json_file)
                 for sentence in self.temp_list:
                     for new_word in sentence:
-                        if new_word[:1].isupper() or new_word.isdigit() and new_word in existing_words['words']:
+                        if (new_word[:1].isupper() or new_word.isdigit()) and new_word in existing_words['words']:
                             add_count = existing_words['words'][new_word] + 1
                             self.all_data['words'][new_word] = add_count
                         else:
